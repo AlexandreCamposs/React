2f53df980fad4593c92de52acae5e26fe2ce740c
AleCampos <alemdcamposs@gmail.com>
2023-05-23 21:16:32 -0300

    requisiçoes

---
+++ 7_req_http/httpreact/src/App.js
@@ -0,0 +1,109 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+// 4 - custom hook
+import { useFetch } from './hooks/useFetch';
+
+const url = 'http://localhost:3000/products';
+
+function App() {
+  const [products, setProducts] = useState([]);
+  // 4 - custom hook
+  const { data: items, httpConfig, loading, error } = useFetch(url);
+
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+
+  // 1 - resgatando dados GET
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const res = await fetch(url);
+  //     // console.log(res);
+
+  //     const data = await res.json();
+  //     // console.log(data);
+  //     setProducts(data);
+  //   }
+  //   fetchData();
+  // }, []);
+
+  // 2- add de produtos POST
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const product = {
+      name,
+      price,
+    };
+
+    // const res = await fetch(url, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json',
+    //   },
+    //   body: JSON.stringify(product),
+    // });
+
+    // // 3 - carregamento dinâmico
+    // const addedProducts = await res.json();
+
+    // setProducts((prevProducts) => [...prevProducts, addedProducts]);
+
+    // 5 - Refatorando "POST"
+    httpConfig(product, 'POST');
+    setName('');
+    setPrice('');
+  };
+
+  // 8 - delete
+  const handleRemove = (id) => {
+    httpConfig(id, 'DELETE');
+  };
+
+  return (
+    <div className="App">
+      <h1>Lista de produtos</h1>
+      {/* 6 - loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      {!loading && (
+        <ul>
+          {items &&
+            items.map((product) => (
+              <li key={product.id}>
+                {product.name} {product.price}
+                <button onClick={() => handleRemove(product.id)}>
+                  Excluir
+                </button>
+              </li>
+            ))}
+        </ul>
+      )}
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          {/* 7 - state de loading no post */}
+          {loading && <input type="submit" value="Aguarde.." disabled />}
+          {!loading && <input type="submit" value="Criar" />}
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
