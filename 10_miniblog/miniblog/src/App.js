c07d2b3d77e3d0e4acdcba3e5b979296bbcfe721
AleCampos <alemdcamposs@gmail.com>
2023-06-15 20:30:07 -0300

    att

--- 10_miniblog/miniblog/src/App.js
+++ 10_miniblog/miniblog/src/App.js
@@ -3,15 +3,21 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
+        <NavBar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </div>
   );
