4b9df2191a39a4de3b5723e0b79329bc915167e0
AleCampos <alemdcamposs@gmail.com>
2023-06-16 21:21:05 -0300

    att

--- 10_miniblog/miniblog/src/App.js
+++ 10_miniblog/miniblog/src/App.js
@@ -1,24 +1,34 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+// context
+import { AuthProvider } from './context/AuthContext';
+
+// pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <NavBar />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <NavBar />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
